dcc77b0b1431c6c61dafb3f1f0af118f5bb70f07
LucasAssis00 <92377740+LucasAssis00@users.noreply.github.com>
2025-02-06 15:24:22 -0300

    Add files via upload

--- deep_solution.py
+++ deep_solution.py
@@ -4,18 +4,25 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import UnexpectedAlertPresentException, NoAlertPresentException
+import time
 
 # Inicializa o driver do Selenium (no exemplo, estou usando o Edge)
-driver = webdriver.Edge()
+driver = webdriver.Firefox()
 
 try:
     # Navega até a página desejada
     driver.get("https://practice-automation.com/form-fields/")
+    time.sleep(3)
 
     # Preenche o formulário e submete (substitua pelos seus próprios comandos)
     # Exemplo:
-    driver.find_element(By.ID, "name-input").send_keys("Seu Nome")
-    driver.find_element(By.ID, "submit-btn").click()
+    #driver.find_element(By.ID, "name-input").send_keys("Seu Nome")
+    driver.find_element("xpath", '/html[1]/body[1]/div[1]/div[2]/div[1]/div[1]/main[1]/div[1]/article[1]/div[1]/form[1]/label[1]/input[1]').send_keys("Seu Nome")
+
+    time.sleep(5)
+    #driver.find_element(By.ID, "submit-btn").click()
+    driver.find_element("xpath", '/html[1]/body[1]/div[1]/div[2]/div[1]/div[1]/main[1]/div[1]/article[1]/div[1]/form[1]/button[1]').click()
+    time.sleep(1)
 
     # Aguarda o alerta aparecer
     try:
